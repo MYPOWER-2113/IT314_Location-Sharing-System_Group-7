25077e85a854e0a550659c6fc560b17edaa11950
Ronit Jain <202001063@daiict.ac.in>
2023-04-02 10:13:23 +0530

    Addition of Buttons
    Buttons Added in Bottom of the Home Page

--- locatinsharing/lib/homepage.dart
+++ locatinsharing/lib/homepage.dart
@@ -2,21 +2,60 @@ import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
 import 'package:latlong2/latlong.dart' as latLng;
 
-
-
 class MyHomePage extends StatefulWidget {
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  int selectedPage = 0;
+
+  final _pageOptions = [
+    // HomeScreen(),
+    // InboxScreen(),
+    // SignInScreen()
+  ];
+
+  int _currentIndex = 0;
+
   @override
   Widget build(BuildContext context) {
 
     return Scaffold(
       appBar: AppBar(
+        title: Text("Location Sharing App"),
+        actions: <Widget>[
+          IconButton(
+              icon: Icon(Icons.arrow_drop_down_circle_sharp),
+              onPressed: () {
+                print("Button Pressed");
+              }),
 
+        ]
       ),
+
+      bottomNavigationBar: BottomNavigationBar(
+
+        items: [
+          BottomNavigationBarItem(label: 'Home', icon: Icon(Icons.home, size: 30), /* title: Text('Home') */),
+          BottomNavigationBarItem(label: 'Share Location', icon: Icon(Icons.place, size: 30), /* title: Text('Inbox') */),
+          BottomNavigationBarItem(label: 'Friends', icon: Icon(Icons.person_4, size: 30), /* title: Text('Account') */),
+          BottomNavigationBarItem(label: 'Places', icon: Icon(Icons.near_me, size: 30), /* title: Text('Account') */),
+          BottomNavigationBarItem(label: 'SOS', icon: Icon(Icons.emergency_share, size: 30), /* title: Text('Account') */),
+          BottomNavigationBarItem(label: 'Navigation', icon: Icon(Icons.navigation, size: 30), /* title: Text('Account') */),
+        ],
+        selectedItemColor: Colors.cyan,
+        elevation: 5.0,
+        unselectedItemColor: Colors.cyan[800],
+        currentIndex: selectedPage,
+        backgroundColor: Colors.white,
+        onTap: (index){
+          setState(() {
+            selectedPage = index;
+          });
+        },
+      ),
+
       body: FlutterMap(
         options: MapOptions(
           center: latLng.LatLng(23.2156, 72.6369),
--- locatinsharing/lib/main.dart
+++ locatinsharing/lib/main.dart
@@ -14,19 +14,28 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Location Sharing App',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.green,
+        // Define the default brightness and colors.
+        brightness: Brightness.light,
+        primaryColor: Colors.cyan[900],
+
+        // Define the default font family.
+        fontFamily: 'gotham',
+
+        textTheme: const TextTheme(
+          displayLarge: TextStyle(fontSize:40.0, fontWeight: FontWeight.bold),
+          titleLarge: TextStyle(fontSize: 25.0, /*fontStyle: FontStyle.italic*/),
+          bodyMedium: TextStyle(fontSize: 14.0, fontFamily: 'Hind'),
+        ),
       ),
+
+      darkTheme: ThemeData(
+          brightness: Brightness.dark,
+          
+      ),
+
+      themeMode: ThemeMode.system,
       home: MyHomePage(),
     );
   }
