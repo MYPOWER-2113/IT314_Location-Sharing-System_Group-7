0c4b3504853ad8e55c39a5b1171e9bbe5181064c
Vedant <coderv.4503@gmail.com>
2023-04-04 15:19:10 +0530

    solve_error_signin

--- BackEnd/routes/user.route.js
+++ BackEnd/routes/user.route.js
@@ -2,35 +2,40 @@ const express = require('express')
 const User = require('../models/user.model')
 const router = express.Router()
 
-//hello
+
 router.post('/signup',async(req,res)=>{
     const {email,password} = req.body;
 
   try {
-    const userDoc = await User.create({
-      email,
-      password,
-    });
-    res.json(userDoc);
+
+    if(await User.findOne({email}))
+    {
+      res.json({
+                message:'Email is already registered'
+            })   
+    }
+    else
+    {
+        const userDoc = await User.create({
+        email,
+        password,
+        });
+        res.json(userDoc);
+    }
+    
   } catch (e) {
     res.status(422).json(e);
   }
     
 })
 
+
 router.post('/signin', async(req,res)=>{
   const {email,password} = req.body;
   const userDoc = await User.findOne({email});
   if (userDoc) {
-    const passOk = compareSync(password, userDoc.password);
-    if (passOk) {
-      jwt.sign({
-        email:userDoc.email,
-        id:userDoc._id
-      }, jwtSecret, {}, (err,token) => {
-        if (err) throw err;
-        res.cookie('token', token).json(userDoc);
-      });
+    if (password == userDoc.password) {
+      res.json(userDoc)
     } else {
       res.status(422).json('pass not ok');
     }
