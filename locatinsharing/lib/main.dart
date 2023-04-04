773b2cd0f59777baaac0e1ee0cd735cb31a45996
Vedant <coderv.4503@gmail.com>
2023-04-04 19:47:53 +0530

    merged login page with map home page

--- locatinsharing/lib/main.dart
+++ locatinsharing/lib/main.dart
@@ -3,6 +3,7 @@ import 'package:flutter_map/flutter_map.dart';
 import 'package:latlong2/latlong.dart' as latLng;
 // import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import'package:locatinsharing/signin.dart';
 import 'homepage.dart';
 
 late SharedPreferences sharedPreferences;
@@ -44,7 +45,7 @@ class MyApp extends StatelessWidget {
       ),
 
       themeMode: ThemeMode.system,
-      home: MyHomePage(),
+      home: Signin(),
     );
   }
 }
--- locatinsharing/macos/Flutter/GeneratedPluginRegistrant.swift
+++ locatinsharing/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,9 +6,11 @@ import FlutterMacOS
 import Foundation
 
 import location
+import path_provider_foundation
 import shared_preferences_foundation
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   LocationPlugin.register(with: registry.registrar(forPlugin: "LocationPlugin"))
+  PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
 }
