46ee4bdef0d83c293da842b329797ce66ef374c3
SHASH1021 <202001078@daiict.ac.in>
2023-04-25 04:05:19 +0530

    updated the code and added search feature

--- locatinsharing/lib/FriendFamily/Contacts.dart
+++ locatinsharing/lib/FriendFamily/Contacts.dart
@@ -1,14 +1,14 @@
-// import 'package:contacts_service/contacts_service.dart';
+import 'package:contacts_service/contacts_service.dart';
 import 'dart:typed_data';
-
+import '../utlis/constants.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
 import 'package:latlong2/latlong.dart' as latLng;
 // import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:shared_preferences/shared_preferences.dart';
-import 'package:location/location.dart';
-import 'package:permission_handler/permission_handler.dart' as per;
-import 'package:flutter_contacts/flutter_contacts.dart';
+// import 'package:location/location.dart';
+import 'package:permission_handler/permission_handler.dart';
+// import 'package:flutter_contacts/flutter_contacts.dart';
 
 import 'package:locatinsharing/homepage.dart';
 import 'package:locatinsharing/main.dart';
@@ -28,12 +28,20 @@ class Contacts extends StatefulWidget {
 
 class _ContactsState extends State<Contacts> {
 
-  List<Contact>? contacts ;
+  List<Contact>? contacts = [];
+
+  List<Contact> contactsFiltered = [];
+  TextEditingController searchController = TextEditingController();
+
   @override
   void initState() {
     // TODO: implement initState
     super.initState();
-    getContact();
+    askPermissions();
+    // getContact();
+    // searchController.addListener(() {
+    //   filterContacts();
+    // });
   }
 
   //navigation bar
@@ -51,6 +59,40 @@ class _ContactsState extends State<Contacts> {
 
   int _currentIndex = 4;
 
+  String flattenPhoneNumber(String phoneStr) {
+    return phoneStr.replaceAllMapped(RegExp(r'^(\+)|\D'), (Match m) {
+      return m[0] == "+" ? "+" : "";
+    });
+  }
+
+  filterContact() {
+    List<Contact> _contacts = [];
+    _contacts.addAll(contacts!);
+    if (searchController.text.isNotEmpty) {
+      _contacts.retainWhere((element) {
+        String searchTerm = searchController.text.toLowerCase();
+        String searchTermFlattren = flattenPhoneNumber(searchTerm);
+        String contactName = element.displayName!.toLowerCase();
+        bool nameMatch = contactName.contains(searchTerm);
+        if (nameMatch == true) {
+
+          return true;
+        }
+        if (searchTermFlattren.isEmpty){
+             return false;
+        }
+        var phone = element.phones!.firstWhere((p) {
+          String phnFLattered = flattenPhoneNumber(p.value!);
+          return phnFLattered.contains(searchTermFlattren);
+        });
+        return phone.value != null;
+      });
+    }
+    setState(() {
+      contactsFiltered = _contacts;
+    });
+  }
+
   void _onItemTapped(int index) {
     setState(() {
       _currentIndex = index;
@@ -61,140 +103,169 @@ class _ContactsState extends State<Contacts> {
     );
   }
 
-  //contacts
-  // List<Contact> contacts = [];
-
-  // @override
-  // void initState() {
-  //   super.initState();
-  //   getAllContacts();
-  // }
-  //
-  // getAllContacts() async {
-  //   List<Contact> _contacts = (await ContactsService.getContacts()).toList();
-  //   setState(() {
-  //     contacts = _contacts;
-  //   });
-  // }
 
 
-  void getContact() async {
-    if (await FlutterContacts.requestPermission()) {
-      contacts = await FlutterContacts.getContacts(
-          withProperties: true, withPhoto: true);
 
-      setState(() {});
+  Future<void> askPermissions() async {
+    PermissionStatus permissionStatus = await getContactsPermissions();
+    if (permissionStatus == PermissionStatus.granted) {
+      getAllContacts();
+      searchController.addListener(() {
+        filterContact();
+      });
+    } else {
+      handInvaliedPermissions(permissionStatus);
     }
   }
+
+  handInvaliedPermissions(PermissionStatus permissionStatus) {
+    if (permissionStatus == PermissionStatus.denied) {
+      dialogueBox(context, "Access to the contacts denied by the user");
+    } else if (permissionStatus == PermissionStatus.permanentlyDenied) {
+      dialogueBox(context, "May contact does exist in this device");
+    }
+  }
+
+  Future<PermissionStatus> getContactsPermissions() async {
+    PermissionStatus permission = await Permission.contacts.status;
+    if (permission != PermissionStatus.granted &&
+        permission != PermissionStatus.permanentlyDenied) {
+      PermissionStatus permissionStatus = await Permission.contacts.request();
+      return permissionStatus;
+    } else {
+      return permission;
+    }
+  }
+
+  getAllContacts() async {
+    List<Contact> _contacts = await ContactsService.getContacts();
+    setState(() {
+      contacts = _contacts;
+    });
+  }
+
   @override
   // ignore: prefer_const_literals_to_create_immutables
   Widget build(BuildContext context) {
+    bool isSearching = searchController.text.isNotEmpty;
+    bool listItemExit = (contactsFiltered.isNotEmpty || contacts!.isNotEmpty);
     return Scaffold(
-      drawer: const DrawerScreen(),
-      appBar: AppBar(
-        title: Text("Contacts"),
+        drawer: const DrawerScreen(),
+        appBar: AppBar(
+          title: Text("Contacts"),
 
-        actions: <Widget>[
-          IconButton(
-            icon: Icon(Icons.notifications_none, size: 30,),
-            onPressed: () {},
-          ),
-        ],
+          actions: <Widget>[
+            IconButton(
+              icon: Icon(Icons.notifications_none, size: 30,),
+              onPressed: () {},
+            ),
+          ],
 
-        //backgroundColor: Colors.cyan,
-        flexibleSpace: Container(
-          decoration: BoxDecoration(
-            gradient: LinearGradient(
-              colors: [Colors.blueAccent.shade100, Colors.greenAccent.shade200],
-              begin: Alignment.bottomRight,
-              end: Alignment.topLeft,
+          //backgroundColor: Colors.cyan,
+          flexibleSpace: Container(
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+                colors: [
+                  Colors.blueAccent.shade100,
+                  Colors.greenAccent.shade200
+                ],
+                begin: Alignment.bottomRight,
+                end: Alignment.topLeft,
+              ),
             ),
           ),
         ),
-      ),
 
-      bottomNavigationBar: BottomNavigationBar(
-        currentIndex: _currentIndex,
-        selectedItemColor: Colors.teal[900],
-        onTap: _onItemTapped,
+        bottomNavigationBar: BottomNavigationBar(
+          currentIndex: _currentIndex,
+          selectedItemColor: Colors.teal[900],
+          onTap: _onItemTapped,
 
-        items: <BottomNavigationBarItem>[
-          BottomNavigationBarItem(
-            icon: Icon(Icons.home, size: 30,),
-            label: 'Home',
-            backgroundColor: Colors.greenAccent.shade200,
-          ),
+          items: <BottomNavigationBarItem>[
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home, size: 30,),
+              label: 'Home',
+              backgroundColor: Colors.greenAccent.shade200,
+            ),
 
-          BottomNavigationBarItem(
-            icon: Icon(Icons.emergency_share, size: 30,),
-            label: 'SOS Share',
-            backgroundColor: Colors.blueAccent.shade100,
-          ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.emergency_share, size: 30,),
+              label: 'SOS Share',
+              backgroundColor: Colors.blueAccent.shade100,
+            ),
 
-          BottomNavigationBarItem(
-            icon: Icon(Icons.navigation, size: 30,),
-            label: 'Navigation',
-            backgroundColor: Colors.greenAccent.shade200,
-          ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.navigation, size: 30,),
+              label: 'Navigation',
+              backgroundColor: Colors.greenAccent.shade200,
+            ),
 
-          BottomNavigationBarItem(
-            icon: Icon(Icons.share_location_sharp, size: 30,),
-            label: 'ShareLocation',
-            backgroundColor: Colors.blueAccent.shade100,
-          ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.share_location_sharp, size: 30,),
+              label: 'ShareLocation',
+              backgroundColor: Colors.blueAccent.shade100,
+            ),
 
-          BottomNavigationBarItem(
-            icon: Icon(Icons.person_4, size: 30,),
-            label: 'Contacts',
-            backgroundColor: Colors.greenAccent.shade200,
-          ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.person_4, size: 30,),
+              label: 'Contacts',
+              backgroundColor: Colors.greenAccent.shade200,
+            ),
 
-          BottomNavigationBarItem(
-            icon: Icon(Icons.near_me_rounded, size: 30,),
-            label: 'Near Me',
-            backgroundColor: Colors.blueAccent.shade100,
-          ),
-        ],
+            BottomNavigationBarItem(
+              icon: Icon(Icons.near_me_rounded, size: 30,),
+              label: 'Near Me',
+              backgroundColor: Colors.blueAccent.shade100,
+            ),
+          ],
 
-        elevation: 50,
-        selectedFontSize: 15,
-      ),
+          elevation: 50,
+          selectedFontSize: 15,
+        ),
 
-      body: (contacts) == null
-        ? Center(child: CircularProgressIndicator())
-        : ListView.builder(
-          itemCount: contacts!.length,
-        itemBuilder: (BuildContext context, int index){
-          Uint8List? image = contacts![index].photo;
-          String number = (contacts![index].phones.isNotEmpty) ? (contacts![index].phones.first.number) : "--";
-            return ListTile(
-              leading: (contacts![index].photo == null)
-                  ? const CircleAvatar(child: Icon(Icons.person))
-                  : CircleAvatar(backgroundImage: MemoryImage(image!)),
-              title: Text(contacts![index].name.first),
-              subtitle: Text(number),
+        body: contacts?.length == 0
+            ? Center(child: CircularProgressIndicator())
+            : SafeArea(
+              child: Column(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: TextField(
+                      autofocus: true,
+                      controller: searchController,
+                      decoration: InputDecoration(
+                        labelText: "search contact",
+                        prefixIcon: Icon(Icons.search)),
+                    ),
+                  ),
+                  listItemExit == true?
+                  Expanded(
+                    child: ListView.builder(
+                    itemCount: isSearching == true
+                        ? contactsFiltered.length
+                        : contacts?.length,
+                    itemBuilder: (BuildContext context, int index) {
+                      Contact contact = contacts![index];
+                      return ListTile(
+                        title: Text(contacts![index].displayName!),
+                          subtitle: Text(contact.phones!.first.value!),
+                        leading: contact.avatar!=null && contact.avatar!.isNotEmpty?
+                        CircleAvatar(
+                          backgroundImage: MemoryImage(contact.avatar!),
+                        ): CircleAvatar(
+                          child: Text(contact.initials()),
+                        ),
+                      );
+                    }
+        ),
+                  )
+              : Container(
+                 child: Text("searching"),
+               ),
+    ]
+              ),
+              ),
             );
-        }
-            // ListView.builder(
-            //   shrinkWrap: true,
-            //   itemCount: contacts.length,
-            //   itemBuilder: (context, index) {
-            //     Contact contact = contacts[index];
-            //     // return ListTile(
-            //     //   title: Text(contact.displayName),
-            //     //   subtitle: Text(
-            //     //       contact.phones
-            //     //           .elementAt(0)
-            //     //           .value
-            //     //   ),
-            //     // );
-            //   },
-            // ),
-
-        ));
-
-
   }
+
 }
-
-
