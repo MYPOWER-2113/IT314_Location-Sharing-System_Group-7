d049aa24ebdcb89e1dd35ee3b577c9e81a5bf844
adityask01 <202001115@daiict.ac.in>
2023-04-27 15:17:34 +0530

    Removed redundant code

--- locatinsharing/lib/ShareLoc/ShareLoc.dart
+++ locatinsharing/lib/ShareLoc/ShareLoc.dart
@@ -26,63 +26,13 @@ import 'package:locatinsharing/FriendFamily/Contacts.dart';
 
 import '../../Slide_nav_bar/Slide_Page.dart';
 
-// import 'package:sms/contact.dart';
 import 'package:flutter_sms/flutter_sms.dart';
-
-// import 'package:flutter_s';
-// import 'package:firebase_dynamic_links/firebase_dynamic_links.dart';
-//
-// String baseUrl = "https://LocationSharing.com";
-// String path = "/";
-// double lat = 37.7749;
-// double long = -122.4194;
-//
-// Uri _link = Uri.parse(baseUrl + path + "?lat=$lat&long=$long");
-// // print(_link.toString());
 
 class ShareLocation extends StatefulWidget {
   @override
   State<ShareLocation> createState() => ShareMyLocation();
 }
 
-class People {
-  @override
-  String? _name, _number;
-
-  People(String? name, String? number) {
-    _name = name;
-    _number = number;
-  }
-}
-
-class MenuItem {
-  final String text;
-  final IconData icon;
-
-  const MenuItem({
-    required this.text,
-    required this.icon,
-  });
-}
-
-class MenuItems {
-  static const List<MenuItem> itemFirst = [itemProfile, itemSettings];
-  static const List<MenuItem> itemSecond = [itemSignout];
-  static const itemSettings = MenuItem(text: "Settings", icon: Icons.settings);
-  static const itemProfile = MenuItem(text: "Your Profile", icon: Icons.person);
-  static const itemSignout = MenuItem(text: "Sign Out", icon: Icons.logout);
-}
-
-PopupMenuItem<MenuItem> buildItem(MenuItem item) => PopupMenuItem<MenuItem>(
-      value: item,
-      child: Row(
-        children: [
-          Icon(item.icon, color: Colors.black, size: 20),
-          const SizedBox(width: 12),
-          Text(item.text),
-        ],
-      ),
-    );
 
 class ShareMyLocation extends State<ShareLocation> {
   late Future<List<Contact>> _future;
@@ -107,20 +57,6 @@ class ShareMyLocation extends State<ShareLocation> {
 
   String valueChoose = "Select Contacts";
   String numChoose="+919313601005";
-  int indexx = 0;
-  List ppl=[
-    "Select Contacts",
-    "+919664842368","+917014850582","+916353553548","+919313601005"];
-
-  List listItem = [
-    "Select Contacts",
-    "Vedant",
-    "Ronit",
-    "Hem",
-    "Aditya"
-  ];
-
-  // final _latlong = getCurrentLatLngFromSharedPrefs();
   final long = getCurrentLatLngFromSharedPrefs().longitude,lat = getCurrentLatLngFromSharedPrefs().latitude;
 
   void _sendSMS(String recipitent) async {
@@ -148,17 +84,6 @@ class ShareMyLocation extends State<ShareLocation> {
       appBar: AppBar(
         title: Text("Share Location"),
 
-        actions: <Widget>[
-          PopupMenuButton<MenuItem>(
-            itemBuilder: (context) => [
-              ...MenuItems.itemFirst.map(buildItem).toList(),
-              PopupMenuDivider(),
-              ...MenuItems.itemSecond.map(buildItem).toList(),
-            ],
-          ),
-        ],
-
-        //backgroundColor: Colors.cyan,
         flexibleSpace: Container(
           decoration: BoxDecoration(
             gradient: LinearGradient(
@@ -231,8 +156,8 @@ class ShareMyLocation extends State<ShareLocation> {
           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children:<Widget>[
             Container(
-              // margin: EdgeInsets.all(15.0),
-              // padding: EdgeInsets.all(8.0),
+              margin: EdgeInsets.all(15.0),
+              padding: EdgeInsets.all(8.0),
               decoration:BoxDecoration(
                   borderRadius:BorderRadius.circular(12),
                   color:Colors.grey
@@ -249,7 +174,7 @@ class ShareMyLocation extends State<ShareLocation> {
                     }
 
                     return Padding(
-                      padding: const EdgeInsets.all(0),
+                      padding: const EdgeInsets.all(12.0),
                       child: Column(
                         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                         children: <Widget>[
@@ -258,12 +183,8 @@ class ShareMyLocation extends State<ShareLocation> {
                             style: TextStyle(fontSize: 23),
                           ),
                           SizedBox(
-                            // height: 64,
-                            // width: 400,
+                            width: 400,
                             child: DropdownButton<Contact>(
-
-                              /*onChanged: (user) =>
-                                  setState(() => _selectedUser = user),*/
                                 onChanged: (newvalue) {
                                   setState(() {
                                     _selectedUser = newvalue;
@@ -278,8 +199,7 @@ class ShareMyLocation extends State<ShareLocation> {
                                     value: user,
                                     child: Row(
                                       children: [
-                                        //Image.network(user.avatar),
-                                        Text('${user.displayName} ${user.phones[0]}'),
+                                        Text('${user.displayName} '),
                                       ],
                                     ),
                                   ),
@@ -306,7 +226,6 @@ class ShareMyLocation extends State<ShareLocation> {
                   ),
                   onPressed: (){
                     if(valueChoose != "Select Contacts") {
-                      // _sendSMS(_selectedUser!.phones[0]);
                       _sendSMS(valueChoose);
                       Fluttertoast.showToast(
                           msg: "Location Shared",
@@ -325,126 +244,6 @@ class ShareMyLocation extends State<ShareLocation> {
     );
   }
 
-  /*body: Align(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            const SizedBox(
-              height: 20,
-            ),
-            Padding(
-                padding: EdgeInsets.all(16.10),
-                child: Padding(
-                    padding: const EdgeInsets.all(3.0),
-
-                      child: Column(
-                        children: [
-                          SizedBox(
-                            height: 3,
-                          ),
-                          // ButtonBar(
-                          //   overflowDirection: VerticalDirection.down,
-                          //   overflowButtonSpacing: 5,
-                          //   alignment: MainAxisAlignment.spaceEvenly,
-                          //   children: [
-                          //
-                          //     ElevatedButton(
-                          //         // onPressed: (){ },
-                          //         // color: _hasBeenPressed1 ? Colors.blue : Colors.black,
-                          //         // 3
-                          //         onPressed: () => {
-                          //           setState(() {
-                          //             _hasBeenPressed1 = !_hasBeenPressed1;
-                          //           })
-                          //         },
-                          //         child: Text ('Share Live Locaion')),
-                          //     ElevatedButton(onPressed: (){}, child: Text ('Share Current Locaion'))
-                          //   ],
-                          // ),
-
-                          DropdownButton(
-                            value: valueChoose,
-                            // hint: Te,
-                            hint: Text("Select Contact: "),
-                            dropdownColor: Colors.grey,
-                            icon: Icon(Icons.arrow_drop_down),
-                            elevation: 16,
-                            iconSize: 40,
-                            isExpanded: true,
-                            underline: SizedBox(),
-                            style: TextStyle(color: Colors.black, fontSize: 22),
-                            onChanged: (String? newvalue) {
-                              setState(() {
-                                valueChoose = newvalue!;
-                                // String vl = newvalue!;
-                                // valueChoose = vl.substring(14);
-                                // // valueChoose = newvalue! ;
-                                // numChoose = vl.substring(0,14);
-                              });
-                            },
-                            items: listItem.map((valueItem) {
-                              return DropdownMenuItem<String>(
-                                value: valueItem,
-                                child: Text(valueItem),
-                              );
-                            }).toList(),
-                          ),
-
-
-                          SizedBox(height: 70),
-                          TextButton(
-                              style: TextButton.styleFrom(
-                                  primary: Colors.purpleAccent,
-                                  backgroundColor: Colors.blue, // Background Color
-                                  shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8.0))
-                              ),
-                              onPressed: (){
-                                if(valueChoose != "Select Contacts") {
-                                  _sendSMS(numChoose);
-                                  Fluttertoast.showToast(
-                                      msg: "Location Shared",
-                                      toastLength: Toast.LENGTH_LONG,
-                                      gravity: ToastGravity.CENTER,
-                                      timeInSecForIosWeb: 1,
-                                      backgroundColor: Colors.red,
-                                      textColor: Colors.white,
-                                      fontSize: 10.0
-                                  );
-                                }
-                              }, child: Text("SEND", style: TextStyle(color: Colors.white,fontSize: 25),)),
-                        ],
-                      ),
-                    )),
-            // Padding(
-            //   padding: const EdgeInsets.fromLTRB(0, 120, 0, 0),
-            //   child: TextButton(
-            //       style: TextButton.styleFrom(
-            //           primary: Colors.purpleAccent,
-            //           backgroundColor: Colors.blue, // Background Color
-            //           shape: RoundedRectangleBorder(
-            //               borderRadius: BorderRadius.circular(8.0))),
-            //       onPressed: () {
-            //         if (valueChoose != "Select Contacts") {
-            //           _sendSMS(valueChoose);
-            //           Fluttertoast.showToast(
-            //               msg: "Location Shared",
-            //               toastLength: Toast.LENGTH_LONG,
-            //               gravity: ToastGravity.CENTER,
-            //               timeInSecForIosWeb: 1,
-            //               backgroundColor: Colors.red,
-            //               textColor: Colors.white,
-            //               fontSize: 10.0);
-            //         }
-            //       },
-            //       child: Text(
-            //         "SEND",
-            //         style: TextStyle(color: Colors.white, fontSize: 25),
-            //       )),
-            // )
-          ],
-        ),
-      ),*/
   Future<List<Contact>> getContacts() async {
     bool isGranted = await Permission.contacts.status.isGranted;
     if (!isGranted) {
